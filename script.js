98bd2eaa9adf45b2e297608eb12db5b2bde52b3b
IshitaKanwar04 <kanwar.ishita04@gmail.com>
2022-03-23 01:00:42 +0530

    added display changes

--- script.js
+++ script.js
@@ -1,16 +1,69 @@
 class Calc{
-    constructor(prevOperand,currOperand){
-        this.prevOperand=prevOperand;
-        this.currOperand=currOperand;
+    constructor(prevOperandTextElement,currOperandTextElement){
+        
+        this.prevOperandTextElement=prevOperandTextElement;
+        this.currOperandTextElement=currOperandTextElement;
+        this.clear();
+
+    }
+
+    clear(){
+        this.prevOperand = '';
+        this.currOperand = '';
+        this.operation = undefined
+    }
+     delete(){
+
+     }
+
+     addNumberToScreen(number){
+         if(number == '.' && this.currOperand.includes('.')) return
+        this.currOperand = this.currOperand.toString() + number.toString();
+     }
+
+     chooseOperation(operation){
+        if(this.currOperand==='') return
+        if(this.prevOperand !== ''){
+            this.calculate()
+        }
+        this.operation = operation;
+        this.prevOperand=this.currOperand;
+        this.currOperand='';
+     }
+
+     calculate(){
+
+     }
+
+     display(){
+        this.currOperandTextElement.innerText = this.currOperand
+        this.prevOperandTextElement.innerText = this.prevOperand   
     }
 }
 
-const numButtons = document.querySelectorAll(['data-number']);
-const opButtons = document.querySelectorAll(['data-operations']);
-const equalsButton = document.querySelector(['data-equals']);
-const deleteButton = document.querySelector(['data-delete']);
-const allClearButton = document.querySelector(['data-all-clear']);
-const prevOperand = document.querySelector('[data-prev-operand]');
-const currOperand = document.querySelector('[data-curr-operand]');
+const numButtons = document.querySelectorAll('[data-number]');
+const opButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const prevOperandTextElement = document.querySelector('[data-prev-operand]');
+const currOperandTextElement = document.querySelector('[data-curr-operand]');
+
+const calc = new Calc(prevOperandTextElement,currOperandTextElement);
 
 
+numButtons.forEach(button =>{
+    button.addEventListener('click',()=> {
+        calc.addNumberToScreen(button.innerText);
+        
+        calc.display();
+    })
+})
+
+opButtons.forEach(button =>{
+    button.addEventListener('click',()=> {
+        calc.chooseOperation(button.innerText);
+        
+        calc.display();
+    })
+})
